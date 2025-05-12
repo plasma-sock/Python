4cb169cf1ede17d6190a8dd00c48ad3d2d6cc763
Sam Nussio <snussio@keyschool.org>
2025-05-12 11:04:10 -0400

    co2_emission.py requests to httpx status

--- web_programming/co2_emission.py
+++ web_programming/co2_emission.py
@@ -4,20 +4,20 @@ Get CO2 emission data from the UK CarbonIntensity API
 
 from datetime import date
 
-import requests
+import httpx
 
 BASE_URL = "https://api.carbonintensity.org.uk/intensity"
 
 
 # Emission in the last half hour
 def fetch_last_half_hour() -> str:
-    last_half_hour = requests.get(BASE_URL, timeout=10).json()["data"][0]
+    last_half_hour = httpx.get(BASE_URL, timeout=10).json()["data"][0]
     return last_half_hour["intensity"]["actual"]
 
 
 # Emissions in a specific date range
 def fetch_from_to(start, end) -> list:
-    return requests.get(f"{BASE_URL}/{start}/{end}", timeout=10).json()["data"]
+    return httpx.get(f"{BASE_URL}/{start}/{end}", timeout=10).json()["data"]
 
 
 if __name__ == "__main__":
